f6ee3ccf912ca53cbd643b660da4aad354d1a4a5
Eimantas Zilis <zilis.eimantas@gmail.com>
2019-07-19 08:34:27 +0100

    Remove stopwords and lemmatize words

--- data/tables.py
+++ data/tables.py
@@ -1,7 +1,13 @@
-import pandas as pd
-from file.management import Directory
+import os
+from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+import pandas as pd
+from file.management import Directory
+
+STOP_WORDS = set(stopwords.words('english'))
+LEM = WordNetLemmatizer()
 
 class Sentences:
     """ Use file.management.Directory object to
@@ -26,9 +32,14 @@ class Sentences:
     def check_directory(self, path):
         """ Check directory and process files within"""
         dir = Directory(path)
+        print("Processing data files...")
         for file in dir.get_files():
+            print(" >> {}".format(os.path.join(path, file.filename)))
             self.tokenize_sentences(file)
             self.tokenize_words(file)
+            self.remove_stopwords(file)
+            self.apply_lematizing(file)
+            self.files.append(file)
 
     @staticmethod
     def tokenize_sentences(file):
@@ -41,3 +52,15 @@ class Sentences:
     @staticmethod
     def tokenize_words(file):
         file.words = [word_tokenize(words) for words in file.sentences]
+
+    @staticmethod
+    def remove_stopwords(file):
+        """ Strip away stopwords from words attribute in file"""
+        file.words = [[w.lower() for w in words if w.lower() not in STOP_WORDS]
+                       for words in file.words]
+    @staticmethod
+    def apply_lematizing(file):
+        """ Reduces words to their word root word
+        or chops off the derivational affixes"""
+        file.words = [[LEM.lemmatize(w, "v") for w in words]
+                       for words in file.words]
