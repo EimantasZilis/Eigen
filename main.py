d0ccd02fac8c065e3c88d7c7f940a788f4d8bf89
Eimantas Zilis <zilis.eimantas@gmail.com>
2019-07-21 17:00:25 +0100

    New -O user input parameter - specify output CSV directory
    And other minor bug fixes

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import argparse
+import os.path
 import data
 from nltk import download
 
 def cmd_parser():
     """ Initialise and define user input commands """
     parser = argparse.ArgumentParser(description='Create hashtags')
-    parser.add_argument("-i",  action="store_true", default=False, dest="install",
+    parser.add_argument("-i", action="store_true", default=False, dest="install",
                         help="Install relevant nltk packages")
+    parser.add_argument("-o", nargs="+", dest="output", help="Set \
+                        directory for output CSV. Default path is user desktop.")
     parser.add_argument("-p", nargs="+", dest="paths", help="Enter paths. \
                         It will find all files within and generates hashtags")
     return parser.parse_args()
@@ -19,13 +22,18 @@ def main():
         download('stopwords')
         download('wordnet')
 
+    if cmd.output:
+        output_path = cmd.output[0]
+    else:
+        output_path = os.path.expanduser("~\\Desktop")
+
     if cmd.paths:
         text = data.Text(cmd.paths)
         text.import_data()
         text.clean()
         text.analyse()
         common_words = text.most_common_words(20)
-        data.Output(text, common_words)
+        data.Output(text, common_words, output_path)
 
 if __name__ == "__main__":
     main()
